6181fad9e6bc738a2605a41c389317eed40d215d
Setric muholo <muholosetric@gmail.com>
2023-06-01 14:23:20 +0000

    ALX SE portfolio project

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleLogin() {
+    var loginSection = document.getElementById("login");
+    loginSection.style.display = loginSection.style.display === "none" ? "block" : "none";
+  }
+  
+  function showLogin() {
+    alert("This feature is not implemented yet.");
+  }
